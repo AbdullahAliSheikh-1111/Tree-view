ffb264fe71dba876f8e975145ae55bf6ee92ff78
AbdullahAliSheikh-1111 <mrbulla602@gmail.com>
2026-08-11 22:26:15 +0500

    Add project files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import TreeView from "./components/tree-view";
+
+
+function App() {
+  return (
+    <div className="App">
+
+       <TreeView menus={menus} /> 
+
+    </div>
+  );
+}
+
+export default App;
